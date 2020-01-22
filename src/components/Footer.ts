20f7d7017d74a657b1cbd2758ce3d5b0659ad12d
Brian Pass <brian.pass7780870@outlook.com>
2020-01-23 00:00:00 +0200

    main: integrate decentralized oracle service

--- src/components/Footer.ts
+++ src/components/Footer.ts
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-main: optimize gas consumption 2020-02-13 00:00:03 +0200
-=======
-user-story: add user analytics 2020-02-13 00:00:05 +0200
->>>>>>> user-story
-=======
-feature: add decentralized lending protocol 2020-01-22 00:00:05 +0200
->>>>>>> feature
+main: integrate decentralized oracle service 2020-01-23 00:00:00 +0200
--- src/config/BlockchainConfig.ts
+++ src/config/BlockchainConfig.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-test: add main branch unit tests 2020-01-22 00:00:01 +0200
-=======
-docs: document new feature in README 2020-02-13 00:00:00 +0200
->>>>>>> feature
+main: integrate decentralized oracle service 2020-01-23 00:00:00 +0200
