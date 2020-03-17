12568b3a0040f12c92ae35882edb19b0c2f81ca8
shoftwarrior <149998544+shoftwarrior@users.noreply.github.com>
2020-03-18 00:00:03 +0200

    user-story: improve user onboarding

--- docs/sprint-backlog.md
+++ docs/sprint-backlog.md
@@ -1,1 +1,1 @@
-user-story: improve user dashboard 2020-04-12 00:00:07 +0200
+user-story: improve user onboarding 2020-03-18 00:00:03 +0200
--- src/pages/contact.ts
+++ src/pages/contact.ts
@@ -1,1 +1,1 @@
-user-story: implement user notifications 2020-04-11 00:00:02 +0200
+user-story: improve user onboarding 2020-03-18 00:00:03 +0200
