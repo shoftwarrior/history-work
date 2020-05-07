9932ce859f9a4a1b210ed29eb6ee51ba77cc7ef0
shoftwarrior <149998544+shoftwarrior@users.noreply.github.com>
2020-05-08 00:00:00 +0200

    feature: implement additional fixes related to bug

--- src/components/BurnDownChart.ts
+++ src/components/BurnDownChart.ts
@@ -1,1 +1,1 @@
-docs: update bugfix documentation 2020-05-20 00:00:03 +0200
+feature: implement additional fixes related to bug 2020-05-08 00:00:00 +0200
--- src/contracts/Crowdsale.sol
+++ src/contracts/Crowdsale.sol
@@ -1,1 +1,1 @@
-refactor: improve code related to bugfix 2020-06-28 00:00:05 +0200
+feature: implement additional fixes related to bug 2020-05-08 00:00:00 +0200
