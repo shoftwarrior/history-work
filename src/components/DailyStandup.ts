936afb0eb441cb41fbd324d542bb01dd051524ea
shoftwarrior <149998544+shoftwarrior@users.noreply.github.com>
2020-05-25 00:00:06 +0200

    feature: enable flash loans

--- src/components/DailyStandup.ts
+++ src/components/DailyStandup.ts
@@ -1,1 +1,1 @@
-merge: integrate changes from user-story branch 2020-05-24 00:00:06 +0200
+feature: enable flash loans 2020-05-25 00:00:06 +0200
--- src/pages/about.ts
+++ src/pages/about.ts
@@ -1,1 +1,1 @@
-feat: introduce dark mode 2020-05-24 00:00:01 +0200
+feature: enable flash loans 2020-05-25 00:00:06 +0200
