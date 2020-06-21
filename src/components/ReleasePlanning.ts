e21815699e890884c4342b6a816a8e61fa42ef97
Yvonne Archibald <yvonne.archibald7033556@aol.com>
2020-06-22 00:00:01 +0200

    bugfix: address issues reported in feature

--- src/components/ReleasePlanning.ts
+++ src/components/ReleasePlanning.ts
@@ -1,1 +1,1 @@
-refactor: optimize code for performance 2020-06-19 00:00:08 +0200
+bugfix: address issues reported in feature 2020-06-22 00:00:01 +0200
--- src/pages/portfolio.ts
+++ src/pages/portfolio.ts
@@ -1,1 +1,1 @@
-feature: integrate decentralized exchange 2020-06-19 00:00:02 +0200
+bugfix: address issues reported in feature 2020-06-22 00:00:01 +0200
