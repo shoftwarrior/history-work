f6abe2ca0f06f91085842e5dfb9cac961429705d
shoftwarrior <149998544+shoftwarrior@users.noreply.github.com>
2020-06-28 00:00:08 +0200

    refactor: optimize database queries

--- src/pages/contact.ts
+++ src/pages/contact.ts
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-style: format code according to style guide 2020-06-23 00:00:06 +0200
-=======
-feature: integrate staking mechanism 2020-06-23 00:00:07 +0200
->>>>>>> feature
-=======
-user-story: improve user onboarding 2020-07-20 00:00:03 +0200
->>>>>>> user-story
+refactor: optimize database queries 2020-06-28 00:00:08 +0200
