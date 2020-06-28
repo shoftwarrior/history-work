6b708ca78dcc437bbb296ab87a620f28d5cb2b7a
shoftwarrior <149998544+shoftwarrior@users.noreply.github.com>
2020-06-29 00:00:06 +0200

    feature: implement user authentication

--- src/components/AgileBoard.ts
+++ src/components/AgileBoard.ts
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-style: format code according to style guide 2020-06-23 00:00:06 +0200
-=======
-feature: integrate staking mechanism 2020-06-23 00:00:07 +0200
->>>>>>> feature
-=======
-user-story: optimize user experience 2020-06-24 00:00:01 +0200
->>>>>>> user-story
-=======
-feature: enable decentralized identity verification 2020-06-29 00:00:00 +0200
->>>>>>> feature
+feature: implement user authentication 2020-06-29 00:00:06 +0200
--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,1 +1,1 @@
-main: implement basic DeFi functionalities 2020-06-29 00:00:05 +0200
+feature: implement user authentication 2020-06-29 00:00:06 +0200
