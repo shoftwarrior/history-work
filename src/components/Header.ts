904b3c918342e0e1d3b3eadcfef4606b14c68da1
Brandi Samuels <brandi.samuels4227284@gmail.com>
2020-07-29 00:00:01 +0200

    docs: document main branch changes

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-feature: implement user authentication 2020-08-09 00:00:03 +0200
-=======
-config: update feature-specific settings 2020-08-13 00:00:03 +0200
->>>>>>> feature
+docs: document main branch changes 2020-07-29 00:00:01 +0200
--- src/contracts/Crowdsale.sol
+++ src/contracts/Crowdsale.sol
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-refactor: optimize database queries 2020-08-09 00:00:04 +0200
-=======
-feature: add search functionality 2020-08-07 00:00:01 +0200
->>>>>>> feature
+docs: document main branch changes 2020-07-29 00:00:01 +0200
