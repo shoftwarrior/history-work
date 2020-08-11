3f4acf8209e8f4ce4877651b1778a5eb2bd2e0a1
Brandi Samuels <brandi.samuels4227284@gmail.com>
2020-08-12 00:00:00 +0200

    bugfix: fix issue with form validation

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-ci: configure continuous integration 2020-08-23 00:00:00 +0200
-=======
-ui: address UI glitches in bugfix 2020-08-24 00:00:05 +0200
->>>>>>> bugfix
+bugfix: fix issue with form validation 2020-08-12 00:00:00 +0200
