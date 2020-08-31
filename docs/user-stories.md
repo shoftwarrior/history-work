f1a77bb1d65d0308973064b7b8a67f19c0dc8d88
shoftwarrior <149998544+shoftwarrior@users.noreply.github.com>
2020-09-01 00:00:05 +0200

    ci: configure continuous integration pipeline

--- docs/user-stories.md
+++ docs/user-stories.md
@@ -1,1 +1,1 @@
-ui: enhance homepage layout 2020-09-01 00:00:02 +0200
+ci: configure continuous integration pipeline 2020-09-01 00:00:05 +0200
--- src/components/DailyStandup.ts
+++ src/components/DailyStandup.ts
@@ -1,1 +1,1 @@
-docs: document main branch changes 2020-09-01 00:00:04 +0200
+ci: configure continuous integration pipeline 2020-09-01 00:00:05 +0200
