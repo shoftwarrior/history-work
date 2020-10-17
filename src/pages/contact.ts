53af924fdf2bfa7844488ecbe366d4364117da2b
Brandi Samuels <brandi.samuels4227284@gmail.com>
2020-10-18 00:00:01 +0200

    main: set up smart contract infrastructure

--- src/pages/contact.ts
+++ src/pages/contact.ts
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-main: set up smart contract infrastructure 2020-10-21 00:00:00 +0200
-=======
-docs: document new feature in README 2020-10-21 00:00:01 +0200
->>>>>>> feature
-=======
-test: write integration tests for feature 2020-10-22 00:00:03 +0200
->>>>>>> feature
+main: set up smart contract infrastructure 2020-10-18 00:00:01 +0200
