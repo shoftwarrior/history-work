e701b8a699b03bc989022d16366b51b7df980e69
shoftwarrior <149998544+shoftwarrior@users.noreply.github.com>
2020-10-27 00:00:04 +0200

    feature: add search functionality

--- src/components/BurnDownChart.ts
+++ src/components/BurnDownChart.ts
@@ -1,1 +1,1 @@
-test: write integration tests for feature 2020-10-27 00:00:01 +0200
+feature: add search functionality 2020-10-27 00:00:04 +0200
--- src/components/TokenTransfer.ts
+++ src/components/TokenTransfer.ts
@@ -1,1 +1,1 @@
-feature: implement synthetic asset creation 2020-11-22 00:00:06 +0200
+feature: add search functionality 2020-10-27 00:00:04 +0200
