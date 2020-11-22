829bce6a9bbd2487ce3f9380591654b8388b7fef
shoftwarrior <149998544+shoftwarrior@users.noreply.github.com>
2020-11-23 00:00:14 +0200

    config: update deployment settings

--- src/components/ReleasePlanning.ts
+++ src/components/ReleasePlanning.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-hotfix: address performance issue 2020-11-23 00:00:06 +0200
-=======
-refactor: improve code related to bugfix 2020-11-23 00:00:09 +0200
->>>>>>> bugfix
+config: update deployment settings 2020-11-23 00:00:14 +0200
