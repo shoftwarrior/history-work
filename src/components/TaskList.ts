e5e3cae3cad6e52018176b4bb8f8ac876923036b
Vincent Gerald <vincent.gerald4577824@gmail.com>
2023-05-30 15:12:16 +0200

    main: update documentation

--- src/components/TaskList.ts
+++ src/components/TaskList.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-chore: initial project setup 2023-06-12 15:12:18 +0200
-=======
-user-story: optimize gas fees for users 2023-05-30 15:12:12 +0200
->>>>>>> user-story
+main: update documentation 2023-05-30 15:12:16 +0200
