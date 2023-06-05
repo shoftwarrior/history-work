e0bc80309aa6ca8634d8d7a4a53a5161cc934105
Adriana Alberts <adriana.alberts1041547@gmail.com>
2023-06-05 15:12:13 +0200

    ci: configure continuous integration

--- src/components/ReleasePlanning.ts
+++ src/components/ReleasePlanning.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-main: optimize gas consumption 2023-06-04 15:12:14 +0200
-=======
-ui: implement new dashboard design 2023-06-04 15:12:18 +0200
->>>>>>> feature
+ci: configure continuous integration 2023-06-05 15:12:13 +0200
