6c7a7b39c422abe6985c46c267cfea7443bcb722
shoftwarrior <149998544+shoftwarrior@users.noreply.github.com>
2023-07-04 15:12:09 +0200

    docs: update project documentation

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-merge: integrate changes from feature branch 2023-07-31 15:12:15 +0200
-=======
-feature: enable decentralized identity verification 2023-07-31 15:12:17 +0200
->>>>>>> feature
+docs: update project documentation 2023-07-04 15:12:09 +0200
