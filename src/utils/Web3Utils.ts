a40eeba0509558ffb2597a7d3d9744f31c36cb46
Vincent Gerald <vincent.gerald4577824@gmail.com>
2023-09-18 15:12:15 +0200

    main: initialize DeFi project

--- src/utils/Web3Utils.ts
+++ src/utils/Web3Utils.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-main: implement basic DeFi functionalities 2023-09-18 15:12:12 +0200
-=======
-hotfix: fix tokenomics issue affecting users 2023-09-21 15:12:11 +0200
->>>>>>> hotfix
+main: initialize DeFi project 2023-09-18 15:12:15 +0200
