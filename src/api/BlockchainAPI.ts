fb81e90b7639e4fc551e90dfc09e4041725e2a67
shoftwarrior <149998544+shoftwarrior@users.noreply.github.com>
2023-11-02 15:12:12 +0200

    feat: introduce dark mode

--- src/api/BlockchainAPI.ts
+++ src/api/BlockchainAPI.ts
@@ -1,1 +1,1 @@
-feature: implement yield farming mechanism 2023-10-31 15:12:12 +0200
+feat: introduce dark mode 2023-11-02 15:12:12 +0200
--- src/components/TaskList.ts
+++ src/components/TaskList.ts
@@ -1,1 +1,1 @@
-feature: integrate decentralized exchange 2023-11-07 15:12:15 +0200
+feat: introduce dark mode 2023-11-02 15:12:12 +0200
