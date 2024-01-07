0c975bb7240e496ad4e06d208c91f8e3ce894674
Vincent Gerald <vincent.gerald4577824@gmail.com>
2024-01-07 15:12:16 +0200

    main: refactor smart contract logic

--- src/utils/Wallet.ts
+++ src/utils/Wallet.ts
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-version: bump to v1.1.0 2024-01-06 15:12:16 +0200
-=======
-feat: develop user profile page 2024-01-05 15:12:10 +0200
->>>>>>> feature
-=======
-user-story: implement user notifications 2024-01-10 15:12:09 +0200
->>>>>>> bugfix
+main: refactor smart contract logic 2024-01-07 15:12:16 +0200
